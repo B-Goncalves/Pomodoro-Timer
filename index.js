1a4cdf92d500740470537f2575b53f3968042791
bruno <70443267+B-Goncalves@users.noreply.github.com>
2024-01-18 20:20:12 -0400

    modificações no html css e na logica do js

--- index.js
+++ index.js
@@ -3,30 +3,55 @@ let seconds = 5;
 let pomodoro;
 let pomodoroCount = 0;
 
+displayRefresh();
 
+document.getElementById('btnSession').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+  });
+  
+  document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+  });
+  
+  document.getElementById('submitModal').addEventListener('click', function() {
+    let minuteSession = document.getElementById('minuteSession').value;
+    let secodsSession = document.getElementById('secodsSession').value;
+    console.log('Minutos:', minuteSession);
+    console.log('Segundos:', secodsSession);
 
-function btnStart(){
+    document.getElementById('myModal').style.display = 'none';
+    minutes = minuteSession;
+    seconds = secodsSession;
+    displayRefresh();
+
+  });
+  
+  function btnStart(){
+    displayRefresh();
     
     pomodoro = setInterval(()=>{
         if(minutes === 0 && seconds === 0){
             pomodoroCount++;
             clearInterval(pomodoro);
         }else if(seconds === 0){
-            seconds = 60;
+            seconds = 59;
             minutes--;
+        } else{
+            seconds--;
         }
-        seconds--;
         displayRefresh();
         
     }, 1000);
-;
 }
+
+
 
 function btnPause(){
     clearInterval(pomodoro);
 }
 
-function displayRefresh(){
+function displayRefresh(){  
+
     const minutesDisplay = minutes < 10 ? '0' + minutes : minutes;
     const secondsDisplay = seconds < 10 ? '0' + seconds : seconds;
 
